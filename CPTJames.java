d81341f95c2337770d702144f138aacab373b6ea
James-Au811 <BoYan.Au26@ycdsbk12.ca>
2025-05-23 09:32:43 -0400

    Github commit test

--- CPTJames.java
+++ CPTJames.java
@@ -4,5 +4,9 @@ public class CPTJames{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+		int intNum;
+		con.println("Give me a number");
+		intNum = con.readInt();
+		con.println(intNum);
 	}
 }
