1978d4834d53943db6177dbdbc125f9f89542998
James-Au811 <BoYan.Au26@ycdsbk12.ca>
2025-06-04 09:27:55 -0400

    Working on putting selected theme choice into array
    When user picks a theme, it will put all of the words into a 2d array and bubble sort them

--- CPTJames.java
+++ CPTJames.java
@@ -45,6 +45,62 @@ public class CPTJames{
 		String strUName;
 		con.println("What is your name?");
 		strUName = con.readLine();
+		
+		//Choosing Theme
+		TextInputFile themes = new TextInputFile("themes.txt");
+		String strTheme;
+		String strChosenTheme;
+		con.println("Choose your theme: ");
+		while(themes.eof()==false){
+			strTheme = themes.readLine();
+			con.println(strTheme);
+		}
+		strChosenTheme = con.readLine();
+		
+		//If Statement for Theme Choice
+		String strF;
+		strF = "Food";
+		String strS;
+		strS = "Sports";
+		String strJ;
+		strJ = "Jobs";
+		
+		String strGameWord[][];
+		int intRand;
+		String strWord;
+		int intRow;
+		int intColumn;
+		intRow = 0;
+		intColumn = 0;
+		if(strChosenTheme.equals(strF)){
+			TextInputFile food = new TextInputFile("foods.txt");
+			strGameWord = new String [intRow][intColumn];
+			String strFood;
+			while(food.eof()==false){
+				strFood = food.readLine();
+				intRand = (int)(Math.random()*100+1);
+				strGameWord[intRow][intColumn] = strFood;
+				intColumn = intColumn + 1;
+				con.println(strGameWord[intRow][intColumn]);
+				strGameWord [intRow][intColumn] = intRand + "";
+				intRow = intRow + 1;
+				con.println(strGameWord[intRow][intColumn]);
+			
+			}
+			
+		}
+			
+		//Putting User into leaderboard
+		String strLeaderboard[][];
+		int intCount;
+		int intUScore;
+		intCount = 0;
+		intUScore = 0; 
+		strLeaderboard = new String[100000][2];
+		strLeaderboard[intCount][intUScore] = strUName;
+		intCount = intCount + 1;
+		
+		
 		con.clear();
 		con.fillRect(0,0,1280,720);
 		
